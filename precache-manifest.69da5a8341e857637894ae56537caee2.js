9eaec0fdcfe1e684a0960a0db11dd360d70d52d4
maryam moradi <moradi.maryam7778@gmail.com>
2020-10-28 22:55:07 +0330

    Updates

---
+++ precache-manifest.69da5a8341e857637894ae56537caee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de07178c716c4f962df0",
+    "url": "/todolist/static/css/main.a8571b4a.chunk.css"
+  },
+  {
+    "revision": "de07178c716c4f962df0",
+    "url": "/todolist/static/js/main.de07178c.chunk.js"
+  },
+  {
+    "revision": "b3f674a911b7829df554",
+    "url": "/todolist/static/js/1.b3f674a9.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "5580c77c3b887f5c942112c5fbb9eeb8",
+    "url": "/todolist/index.html"
+  }
+];
